3b94cff0a4b3befcb2e21169ec4d2ceea6b62beb
Pat Rocchio <patrocc6@gmail.com>
2015-07-18 18:09:22 +0000

    Get template working

--- Gemfile
+++ Gemfile
@@ -30,6 +30,12 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+#Font awesome gem
+gem 'font-awesome-rails'
+
+#Fancybox gem
+gem 'fancybox2-rails', '~> 0.2.8'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,15 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( modernizr-latest.js )
+Rails.application.config.assets.precompile += %w( jquery-1.8.2.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( jquery.isotope.min.js )
+Rails.application.config.assets.precompile += %w( jquery.nav.js )
+Rails.application.config.assets.precompile += %w( jquery.cslider.js )
+Rails.application.config.assets.precompile += %w( custom.js )
+Rails.application.config.assets.precompile += %w( owl-carousel/owl.carousel.js )
+Rails.application.config.assets.precompile += %w( fancybox/jquery.fancybox.pack.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
