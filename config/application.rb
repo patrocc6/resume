612c2ca95bbeb8df82028f7cd457c12f45c546b9
Pat Rocchio <patrocc6@gmail.com>
2015-07-26 19:59:26 +0000

    Fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -25,4 +25,5 @@ module Resume
     config.action_mailer.delivery_method = :sendmail
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
+  end
 end
