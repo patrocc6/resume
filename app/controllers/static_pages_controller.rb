2b3d6f70cd8e6bb683475236368a1f993058c6ed
Pat Rocchio <patrocc6@gmail.com>
2015-07-28 01:10:46 +0000

    Add deliver to mail call

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,7 @@ class StaticPagesController < ApplicationController
     email = params[:email]
     body = params[:comment]
     
-    ContactMailer.contact_email(name, email, body)
+    ContactMailer.contact_email(name, email, body).deliver
     redirect_to "/#contact", notice: 'Message sent!'
   end
 end
